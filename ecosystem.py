e8dcd72d18561e7b5de6859f3854b9fe9100e5b3
HBG <agiphy98@gmail.com>
2026-04-18 11:32:21 -0700

    feat-Buildings Dictionary

--- ecosystem.py
+++ ecosystem.py
@@ -11,10 +11,54 @@ herbivores = 1000,
 carnivores = 100,
 apex_predators = 10
 
+building_dict = {
+    "Factory": {
+        "pollution": 100,
+        "score": 20,
+    },
+    "Park": {
+        "pollution": -20,
+        "score": -5,
+    }
+}
+
+def print_dict(dict):
+    for item in dict:
+        print("_________________________")
+        print(item)
+        print("Pollution: " + str(dict[item]["pollution"]))
+        print("Score: " + str(dict[item]["score"]))
+
+
+def print_stats():
+    print("Score: " + str(score))
+    print("Pollution: " + str(pollution))
+    print("Temperature: " + str(temp))
+    print("Light Level: " + str(light_level))
+    print("Water Level: " + str(water_level))
+    print("========================")
+    print("Plants alive: " + str(plants))
+    print("Herbivores alive: " + str(herbivores))
+    print("Carnivores alive: "  + str(carnivores))
+    print("Apex predators alive: " + str(apex_predators))
+
 while gameover == False:
     # game loop
 
     # player choices
+    while(True):
+        player_input = input("Choose your building (enter 'view' to se choices): ")
+        if(player_input == "view"):
+            print_dict(building_dict)
+            continue
+        try:
+            pollution += building_dict["pollution"]
+            score += building_dict["score"]
+        except KeyError:
+            print("Invalid choice")
+            continue
+
+
     # filler text and display graphics
     print("Score: " + str(score))
 
