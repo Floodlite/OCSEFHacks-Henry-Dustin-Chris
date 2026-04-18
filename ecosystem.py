8eb3822350aeeaf4e8fa251ba8e2d9db44e0c522
Yang Jiao <yang.jiao@oracle.com>
2026-04-18 11:14:32 -0700

    ecosystem framework - Dustin

--- ecosystem.py
+++ ecosystem.py
@@ -1,8 +1,21 @@
 score = 0
 gameover = False
+pollution = 0
+temp = 100
+light_level = 100
+water_level = 100
+organic_matter = 100000
+
+plants = 10000,
+herbivores = 1000,
+carnivores = 100,
+apex_predators = 10
 
 while gameover == False:
     # game loop
+
     # player choices
     # filler text and display graphics
+    print("Score: " + str(score))
+
 # endgame psa
